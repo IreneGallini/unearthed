2a399bf45d3b11e43b655d2631fd37fe6d53ec25
IreneGallini <igallini@macalester.edu>
2026-03-04 19:08:00 -0600

    week1

---
+++ server/data/gifts.js
@@ -0,0 +1,54 @@
+
+// An array of gift objects
+const giftData = [
+  {
+    "id": 1,
+    "name": "Disco Ball Candle",
+    "pricePoint": "$",
+    "audience": "Candle Lovers",
+    "image": "https://paddywax.com/cdn/shop/files/MA1004_3.jpg?v=1759511964&width=416",
+    "description": "A party in a candle",
+    "submittedBy": "Sasha",
+    "submittedOn": "2022-09-04T14:48:00"
+  },
+  {
+    "id": 2,
+    "name": "Galaxy Notebook",
+    "pricePoint": "$$",
+    "audience": "Writers & Students",
+    "image": "https://m.media-amazon.com/images/I/71rr6feS+uL._AC_UF894,1000_QL80_.jpg",
+    "description": "Notebook with a stunning galaxy cover to inspire creativity",
+    "submittedBy": "Jordan",
+    "submittedOn": "2022-10-10T09:30:00"
+  },
+  {
+    "id": 3,
+    "name": "Mini Plant Terrarium",
+    "pricePoint": "$$",
+    "audience": "Plant Enthusiasts",
+    "image": "https://static01.nyt.com/images/2021/11/28/realestate/22garden-terrariums01/oakImage-1637602900312-superJumbo.jpg",
+    "description": "A cute glass terrarium to keep small plants indoors",
+    "submittedBy": "Alex",
+    "submittedOn": "2022-11-15T12:15:00"
+  },
+  {
+    "id": 4,
+    "name": "Reusable Stainless Steel Straw Set",
+    "pricePoint": "$",
+    "audience": "Eco-Conscious",
+    "image": "https://m.media-amazon.com/images/I/71IbwJLHUHL.jpg",
+    "description": "Reduce waste with this set of reusable straws and cleaning brush",
+    "submittedBy": "Riley",
+    "submittedOn": "2022-12-01T08:45:00"
+  },
+  {
+    "id": 5,
+    "name": "LED String Lights",
+    "pricePoint": "$$",
+    "audience": "Home Decor Lovers",
+    "image": "https://m.media-amazon.com/images/I/71nolh8hfDL.jpg",
+    "description": "Decorate any room with warm, cozy LED string lights",
+    "submittedBy": "Morgan",
+    "submittedOn": "2023-01-20T17:00:00"
+  }
+];
